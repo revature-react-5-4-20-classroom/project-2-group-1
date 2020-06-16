3e7dec125eede563e70100ca77822b7e6a22ff20
Dianne Desilets <deedesilets@yahoo.com>
2020-06-16 17:21:46 -0400

    movies/id endpoint working on postman

--- movies/src/main/java/com/revature/movies/services/MovieServices.java
+++ movies/src/main/java/com/revature/movies/services/MovieServices.java
@@ -1,10 +1,13 @@
 package com.revature.movies.services;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.revature.movies.models.Movie;
 import com.revature.movies.repositories.MovieRepository;
+
 
 
 @Service
@@ -18,6 +21,11 @@ public class MovieServices implements MovieServicesInterface {
    return movieRepository.findAllSorted();
   }
   
+  @Override
+  public Optional<Movie> getById(Integer movieid) {
+	  
+	  return movieRepository.findById(movieid);
+  }
   
   
 }
--- movies/src/main/java/com/revature/movies/services/MovieServicesInterface.java
+++ movies/src/main/java/com/revature/movies/services/MovieServicesInterface.java
@@ -1,5 +1,7 @@
 package com.revature.movies.services;
 import java.util.List;
+import java.util.Optional;
+
 import com.revature.movies.models.Movie;
 
 
@@ -7,5 +9,6 @@ import com.revature.movies.models.Movie;
 public interface MovieServicesInterface {
 
  public List<Movie> getAll();
+ public Optional<Movie> getById(Integer id);
  
 }
