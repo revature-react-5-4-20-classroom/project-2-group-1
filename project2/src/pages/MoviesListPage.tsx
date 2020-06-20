9e36304e8c73269f79e6089ac632c2e59ce21131
Ben Worrell <benjamin.worrell.w@gmail.com>
2020-06-20 17:32:57 -0400

    clicking on a preview takes you to the movie page

--- project2/src/pages/MoviesListPage.tsx
+++ project2/src/pages/MoviesListPage.tsx
@@ -42,7 +42,7 @@ export class MoviesListPage extends React.Component<any, any> {
                 //return <img key={movieObj.movieId} src={movieObj.poster}/>
                 return (
                 <Col className="myColumn myPadding" key={movieObj.movieId} xl={6}>
-                    <MoviePreview movie={movieObj} deleteButton={false}/>
+                    <MoviePreview onClick={()=>{this.props.history.push(`title/${movieObj.title}`)}} movie={movieObj} deleteButton={false}/>
                 </Col>)
             })}
         </Row>
