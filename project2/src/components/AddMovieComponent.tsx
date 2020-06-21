3995ce01580ddb6941529972b1429315f05bee60
deep <hereisdanbrown@gmail.com>
2020-06-20 20:11:21 -0600

    Added add movie to user list functionality

---
+++ project2/src/components/AddMovieComponent.tsx
@@ -0,0 +1,120 @@
+import React from 'react';
+import { getAllUserLists, getUserListByListId, getAllMovies, addMovieToUserList, getUserListBy } from '../api/movieClient';
+import { Button } from 'reactstrap';
+import { Movie } from '../models/Movie';
+import { User } from '../models/Users';
+
+
+interface IAddMovieComponentProps {
+  userListsActionMapper: (userLists: any[]) => any;
+  moviesUpdateActionMapper: (movies: Movie[]) => any;
+  movies: Movie[];
+  loggedInUser: User;
+}
+
+export class AddMovieComponent extends React.Component<IAddMovieComponentProps, any> {
+  constructor(props: IAddMovieComponentProps) {
+    super(props);
+    this.state = {
+      value: "0",
+      allMovies: [],
+      listMovieIds: [],
+      userListId: 0,
+      movieAdded: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  async componentDidMount()
+  {
+    try 
+    {
+      let allMovies = await getAllMovies();
+      this.setState({allMovies});
+      // I need to set the global movies to the current users list
+      let { movies, userListId } = await getUserListByListId(this.props.loggedInUser.userId);
+      this.props.moviesUpdateActionMapper(movies);
+      this.setState({userListId})
+      // Then I need all of those ids
+      // If he tries to add a movie from that list then don't do it
+      let listMovieIds = movies.map((movie: Movie) => movie.movieId);
+      this.setState({listMovieIds});
+    }
+    catch(e)
+    {
+      throw(e.message);
+    }
+  }
+
+  handleChange(e: any) {
+    this.setState({value: e.target.value});
+  }
+
+  async handleSubmit(e: any) {
+    e.preventDefault();
+
+    // console.log(this.state.listMovieIds.includes(parseInt(this.state.value)))
+    if (this.state.value == 0) {
+      return;
+    }
+    let movieIds = this.state.listMovieIds;
+    let movieId = this.state.value;
+    let isDuplicate = movieIds.includes(parseInt(movieId));
+    if (!isDuplicate) {
+      console.log("adding movie");
+      let numberMoviesAdded = 0;
+      try 
+      {
+        numberMoviesAdded = await addMovieToUserList(this.state.value, this.state.userListId);
+        if (numberMoviesAdded !== 0) this.setState({movieAdded: true})
+      }
+      catch(e) 
+      {
+        throw(e.message);
+      }
+      if (numberMoviesAdded === 0) return;
+      try 
+      {
+        let { movies } = await getUserListByListId(this.state.userListId);
+        console.log(movies);
+        this.props.moviesUpdateActionMapper(movies);
+      }
+      catch(e)
+      {
+        throw(e.message);
+      }
+      //TODO Later
+      // console.log("just before the timeout")
+      // setTimeout(() => this.setState({numberMoviesDeleted: 0}), 50000)
+      // console.log("after the timeout");
+    }
+  }
+
+  
+
+
+
+  render() {
+    return (
+      <>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+          {`Available Movies: `}
+          <select name="listId" value={this.state.value} onChange={this.handleChange}>
+            <option value={0}>Select A Movie To Add</option>
+            {this.state.allMovies.map((movie: Movie) => 
+              <option 
+              key={movie.movieId}
+              value={movie.movieId}
+              >
+                {movie.title}
+              </option>)}
+          </select>
+          </label>
+          <input type="submit"/>
+        </form>
+      </>
+    )
+  }
+}
--- project2/src/pages/MoviesListFormPage.tsx
+++ project2/src/pages/MoviesListFormPage.tsx
@@ -2,10 +2,29 @@ import React from 'react';
 import { Movie } from '../models/Movie';
 import { User } from '../models/Users';
 import { Form, FormGroup, Label, Col, Input, Button, Toast, ToastHeader, ToastBody, Jumbotron, Row } from 'reactstrap';
-import { getUserListBy, deleteMovieFromUserList, addMovieToUserList } from '../api/movieClient';
+import { getUserListBy, deleteMovieFromUserList, addMovieToUserList, getAllMovies } from '../api/movieClient';
 import { MoviePreview } from '../components/moviePreview';
 import { DBToast } from '../components/DBToast';
+import { UserListsComponent } from '../components/UserListsComponent';
+import { AddMovieComponent } from '../components/AddMovieComponent';
+import { IState } from '../redux/reducers';
+import { moviesUpdateActionMapper, userListsActionMapper } from "../redux/action-mappers";
+import { connect } from 'react-redux';
 
+//! For Redux
+const mapStateToPropsUserLists = (state: IState) => {
+  const { movies } = state.moviesStore;
+  return {
+    movies,
+  }
+}
+const mapDispatchToProps = {
+  moviesUpdateActionMapper,
+  userListsActionMapper
+}
+
+const AddMovieComponentReduxContainer = connect(mapStateToPropsUserLists, mapDispatchToProps)(AddMovieComponent);
+//! End of Redux
 
 interface IMoviesListFormPageProps {
   moviesUpdateActionMapper: (movies: Movie[]) => any;
@@ -15,6 +34,7 @@ interface IMoviesListFormPageProps {
 }
 
 interface IMoviesListFormPageState {
+  allMovies: Movie[];
   listName: string;
   userListId: number;
   numberMoviesDeleted: number;
@@ -25,6 +45,7 @@ export class MoviesListFormPage extends React.Component <IMoviesListFormPageProp
   constructor(props: IMoviesListFormPageProps) {
     super(props);
     this.state = {
+      allMovies: [],
       listName: "",
       userListId: 0,
       numberMoviesAdded: 0,
@@ -38,6 +59,15 @@ export class MoviesListFormPage extends React.Component <IMoviesListFormPageProp
       let { movies, listName, userListId } = await getUserListBy(this.props.loggedInUser.userId);
       this.props.moviesUpdateActionMapper(movies);
       this.setState({listName, userListId})
+    }
+    catch(e)
+    {
+      throw(e.message);
+    }
+    try 
+    {
+      let allMovies = await getAllMovies();
+      this.setState({allMovies})
     }
     catch(e)
     {
@@ -83,37 +113,6 @@ export class MoviesListFormPage extends React.Component <IMoviesListFormPageProp
     console.log("after the timeout");
   }
 
-  addMovieClick = async (e: any) => {
-    console.log("adding movie");
-    // Random number
-    let movieId = Math.ceil(Math.random()*10);
-    let numberMoviesAdded = 0;
-    console.log(movieId);
-    try 
-    {
-      numberMoviesAdded = await addMovieToUserList(movieId, this.state.userListId);
-      if (numberMoviesAdded != this.state.numberMoviesDeleted) this.setState({numberMoviesAdded})
-    }
-    catch(e) 
-    {
-      throw(e.message);
-    }
-    if (numberMoviesAdded === 0) return;
-    try 
-    {
-      let { movies } = await getUserListBy(this.props.loggedInUser.userId);
-      console.log(movies);
-      this.props.moviesUpdateActionMapper(movies);
-    }
-    catch(e)
-    {
-      throw(e.message);
-    }
-    //TODO Later
-    // console.log("just before the timeout")
-    // setTimeout(() => this.setState({numberMoviesDeleted: 0}), 50000)
-    // console.log("after the timeout");
-  }
 
 
   render() {
@@ -122,9 +121,10 @@ export class MoviesListFormPage extends React.Component <IMoviesListFormPageProp
       <div className="page" id="moviesListFormPage">
         <Jumbotron>
           {/* This will take some work to make it a dynamic title so come back later if you have time */}
-          <h1 className="display-3 center">{`Movies List: ${this.state.listName}`}</h1>
+          <h1 className="display-3 center">{`Your Movie List:`}</h1>
+          <h1 className="display-3 center">{`${this.state.listName}`}</h1>
         </Jumbotron>
-        <Button onClick={this.addMovieClick}>Add a random movie</Button>
+        <AddMovieComponentReduxContainer loggedInUser={this.props.loggedInUser}></AddMovieComponentReduxContainer>
         <Row>
             {movies.length === 0 ? "" : movies.map((movieObj: Movie)=>{
                 //return <img key={movieObj.movieId} src={movieObj.poster}/>
